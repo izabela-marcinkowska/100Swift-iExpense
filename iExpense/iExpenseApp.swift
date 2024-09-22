fcb99c1bcd07d8b0f79e0a74c7a67010b9a19175
Devy <izabela.marcinkowska@outlook.com>
2024-09-22 16:23:41 +0200

    First commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Izabela Marcinkowska on 2024-09-22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
