67dd945b3a33cd0ce832e9b31700472eba1119ad
Devy <izabela.marcinkowska@outlook.com>
2024-09-25 10:31:41 +0200

    feat: Add new view

---
+++ iExpense/AddView.swift
@@ -0,0 +1,36 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Izabela Marcinkowska on 2024-09-25.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    let types = ["Personal", "Business"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add Expense")
+        }
+    }
+}
+
+#Preview {
+    AddView()
+}
--- iExpense/ExpenseItem.swift
+++ iExpense/ExpenseItem.swift
@@ -5,7 +5,10 @@
 //  Created by Izabela Marcinkowska on 2024-09-25.
 //
 
-struct ExpenseItem {
+import Foundation
+
+struct ExpenseItem: Identifiable{
+    let id = UUID()
     let name: String
     let type: String
     let amount: Double
