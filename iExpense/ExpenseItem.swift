5b5a4a3ec7d649a1e389d53fdb657d10a72514b8
Devy <izabela.marcinkowska@outlook.com>
2024-09-25 09:24:45 +0200

    feat: delete list items

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,13 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Izabela Marcinkowska on 2024-09-25.
+//
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
