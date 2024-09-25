d316ce23338c34db520d7b0d16539406200fc369
Devy <izabela.marcinkowska@outlook.com>
2024-09-25 11:29:49 +0200

    feat: Use users currency

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -25,7 +25,7 @@ struct AddView: View {
                         Text($0)
                     }
                 }
-                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("Add Expense")
