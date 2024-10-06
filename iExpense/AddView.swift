3420e8267bd5fe6669f3d29bf129274999de0ff0
Devy <izabela.marcinkowska@outlook.com>
2024-10-06 12:01:43 +0200

    feat: Add cancel button to the adding view

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -30,12 +30,17 @@ struct AddView: View {
             }
             .navigationTitle("Add Expense")
             .toolbar {
+                Button("Cancel") {
+                    dismiss()
+                }
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
                     expenses.items.append(item)
                     dismiss()
                 }
+                
             }
+            .navigationBarBackButtonHidden()
         }
     }
 }
