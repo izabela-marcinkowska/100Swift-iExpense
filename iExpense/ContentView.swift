0f103dea8313caea28129d9a1b3d7d9d0f023530
Devy <izabela.marcinkowska@outlook.com>
2024-10-22 18:57:22 +0200

    feat: Show expenses in new view

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -15,40 +15,8 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            List {
-                Section ("Personal Expenses") {
-                    ForEach(expenses) { item in
-                        if(item.type == "Personal") {
-                            HStack {
-                                VStack (alignment: .leading) {
-                                    Text(item.name)
-                                        .font(.headline)
-                                    Text(item.type)
-                                }
-                                Spacer()
-                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                                    .foregroundStyle(item.amount < 100 ? .green : .red)
-                            }
-                        }
-                    }.onDelete(perform: removeItems)
-                }
-                Section ("Business Expenses") {
-                    ForEach(expenses) { item in
-                        if(item.type == "Business") {
-                            HStack {
-                                VStack (alignment: .leading) {
-                                    Text(item.name)
-                                        .font(.headline)
-                                    Text(item.type)
-                                }
-                                Spacer()
-                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                                    .foregroundStyle(item.amount < 100 ? .green : .red)
-                            }
-                        }
-                    }.onDelete(perform: removeItems)
-                }
-            }.navigationTitle("iExpense")
+            ExpenseView(filterType: "Business")
+            .navigationTitle("iExpense")
                 .toolbar {
                     NavigationLink("Add new") {
                         AddView()
---
+++ iExpense/ExpenseView.swift
@@ -0,0 +1,68 @@
+//
+//  ExpenseView.swift
+//  iExpense
+//
+//  Created by Izabela Marcinkowska on 2024-10-22.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ExpenseView: View {
+    @Query var expenses: [Expense]
+    @Environment(\.modelContext) var modelContext
+    
+    init(filterType: String) {
+        _expenses = Query(filter: #Predicate<Expense> { expense in
+            expense.type == filterType
+        })
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        for offset in offsets {
+            let expense = expenses[offset]
+            modelContext.delete(expense)
+        }
+    }
+    
+    var body: some View {
+        List {
+            Section ("Personal Expenses") {
+                ForEach(expenses) { item in
+                    if(item.type == "Personal") {
+                        HStack {
+                            VStack (alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                            }
+                            Spacer()
+                            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                .foregroundStyle(item.amount < 100 ? .green : .red)
+                        }
+                    }
+                }.onDelete(perform: removeItems)
+            }
+            Section ("Business Expenses") {
+                ForEach(expenses) { item in
+                    if(item.type == "Business") {
+                        HStack {
+                            VStack (alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                            }
+                            Spacer()
+                            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                .foregroundStyle(item.amount < 100 ? .green : .red)
+                        }
+                    }
+                }.onDelete(perform: removeItems)
+            }
+        }
+    }
+}
+
+#Preview {
+    ExpenseView(filterType: "Business")
+}
